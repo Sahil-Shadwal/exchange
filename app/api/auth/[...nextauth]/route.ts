74417f173c9b6ce4bf2c60642e5019b2138519bd
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-05 17:04:11 +0530

    created the schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import db from "@/app/db";
+import { Keypair } from "@solana/web3.js";
 
 const handler = NextAuth({
   providers: [
@@ -8,16 +10,53 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  // callbacks: {
-  //   async signIn({ user, account, profile, email, credentials }) {
-  //     if (account?.provider === "google") {
-  //       const email = user.email;
-  //       if (!email) {
-  //         return false;
-  //       }
-  //     }
-  //   },
-  // },
+  callbacks: {
+    // @ts-ignore
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account?.provider === "google") {
+        const email = user.email;
+        if (!email) {
+          return false;
+        }
+
+        console.log({ user, account, profile, email, credentials });
+        const userDb = await db.user.findFirst({
+          where: {
+            username: email,
+          },
+        });
+
+        if (userDb) {
+          return true;
+        }
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toBase58();
+        const privateKey = keypair.secretKey;
+
+        await db.user.create({
+          data: {
+            username: email,
+            name: profile?.name,
+            // @ts-ignore
+            profilePicture: profile?.picture,
+            provider: "Google",
+            solWallet: {
+              create: {
+                publicKey: publicKey,
+                privateKey: privateKey.toString(),
+              },
+            },
+            InrWallet: {
+              create: {
+                balance: 0,
+              },
+            },
+          },
+        });
+        return true;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
